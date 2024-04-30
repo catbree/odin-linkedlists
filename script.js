36cd46db946751380d4ab93d0c756c47987b203a
Hoi Cheng <49464204+catbree@users.noreply.github.com>
2024-04-30 22:37:09 +0800

    turns out i am using array functions so removing the functions i made and restarting...

--- script.js
+++ script.js
@@ -1,36 +1,12 @@
 class LinkedList {
     constructor() {
-        this.nodes = [];
+        this.headNode = null;
     }
-
-    append(value) {
-        const node = new Node(value);
-        this.nodes[this.nodes.length] = node;
-    }
-
-    toString() {
-        let printStatement = '';
-        this.nodes.forEach( (node) => {
-            printStatement += `(${node.value}) -> `
-        });
-        printStatement = printStatement + ` null`;
-        console.log(printStatement);
-    }
-    
-    //insert functions here
 }
 
 class Node {
     constructor(value) {
-        this.value = value;
+        this.value = value | null;
         this.nextNode = null;
     }
-
-    //insert functions here
 }
-
-const linkedlist = new LinkedList();
-linkedlist.append(24);
-linkedlist.append(232);
-linkedlist.append(92);
-linkedlist.toString();
